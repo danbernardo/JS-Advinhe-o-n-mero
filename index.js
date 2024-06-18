9ca3b40001f044224430b3e06af62e87aac50b31
Danbernardo <danielbernardo18sr@email.com>
2024-06-18 15:41:00 -0300

    Conversor de Temperaturas

--- index.js
+++ index.js
@@ -1,46 +1,23 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
 
 
- const minNum = 1;
- const maxNum = 100;
- const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
- console.log(`Número gerado: ${answer}`);
+function convert(){
 
- const enviarId = document.getElementById('enviarId');
- const inputField = document.querySelector('input[name="Nome"]');
- const feedback = document.getElementById('feedback');
-
- const refreshButton = document.getElementById('refreshButton');
- refreshButton.addEventListener('click', function(){
-     location.reload();
- });
-
- let attempts = 0;
- let running = true; 
-
-
- enviarId.addEventListener('click', function() {
-     if (!running) return; 
-     const guess = Number(inputField.value);
-     console.log(`Tentativa do usuário: ${guess}`);
-     attempts++; 
-
-     if (isNaN(guess) || guess < minNum || guess > maxNum ){
-         feedback.textContent = `Por favor, insira um número válido entre ${minNum} e ${maxNum}`;
-     } else if (guess === answer) {
-         feedback.textContent = `Parabéns! Você acertou o número ${answer} em ${attempts} tentativas`;
-         running = false;
-     } else if (guess < answer) {
-         feedback.textContent = 'Tente um número maior';
-     } else {
-         feedback.textContent = 'Tente um número menor';
-     }
-     inputField.value = '';
-     inputField.focus();
- });
-
- refreshButton.addEventListener('click', function(){
-     location.reload()
- });
-
-
+    if(toFahrenheit.checked){
+        temp = Number(textBox.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "ºF";
+    }
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "º";
+    }else{
+        result.textContent = "Selecione a unidade";
+    }
+}
